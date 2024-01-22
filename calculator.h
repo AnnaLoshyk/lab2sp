92c22b83d3da5c7f6f5d4eb13c4da08dc0d35028
AnnaLoshyk <loshykanna@gmail.com>
2024-01-22 03:09:44 +0200

    Add division and percentage

--- calculator.h
+++ calculator.h
@@ -6,9 +6,9 @@ class Calculator
 	public:
 		int Add (double, double);
 		int Sub (double, double);
-	public:
-        	int Add (int, int);
         	int Mul (double, double);
+		double Div (double, double);
+		double Perc (double, double);
 };
 
 #endif//CALCULATOR_H
